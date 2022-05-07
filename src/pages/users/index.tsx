67ec54d87e8ae07bf104c641df6bc73d58135e65
diegobaena89 <diegobf.cello@gmail.com>
2022-05-07 14:49:04 -0300

    add users creation page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,22 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
-import { RiAddLine } from "react-icons/ri";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -22,11 +37,51 @@ export default function UserList() {
               size="sm"
               fontSize="sm"
               colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} />}
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
             >
               Criar novo usuário
             </Button>
           </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="6"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                    <Box>
+                        <Text fontWeight="bold">Diego Baena</Text>
+                        <Text fontSize="sm" color="gray.300">diegobf@gmail.com</Text>
+                    </Box>
+                </Td>
+                <Td>04 de Abril de 2022</Td>
+                <Td>
+                <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="purple"
+              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+            >
+              Editar
+            </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
